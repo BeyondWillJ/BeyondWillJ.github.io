8ae4fdf01c74f104df26ea441313c439404ac38e
BeyondWillJ <jr2020work@gmail.com>
2025-02-02 19:34:32 +0800

    Site updated: 2025-02-02 19:34:28

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,8 @@
-document.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach(function(header) {
-    header.addEventListener('click', function() {
-      window.location.hash = this.id;
-    });
-  });
+// document.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach(function(header) {
+//     header.addEventListener('click', function() {
+//       window.location.hash = this.id;
+//     });
+//   });
 
 
   
